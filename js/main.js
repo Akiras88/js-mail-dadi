a3112adf233a98efcb49b443a2fb6988e5c88717
Francesco <francesco.scarnera3@gmail.com>
2020-03-30 17:19:45 +0200

    check mail

---
+++ js/main.js
@@ -0,0 +1,28 @@
+/**
+ * SEARCH IN AN ARRAY 
+ */
+
+/**
+ * VARIABLE
+ */
+
+var mail = [ 'michele@boolean.careers', 'fabio@boolean.careers', 'roberto@boolean.careers', 
+'francesco@gmail.com', 'giovanni@gmail.com' ];
+var user = prompt('Inserisci la tua Email');
+var userFound = false;
+
+// search
+for (var i = 0; i<mail.length; i++) {
+    var item = mail[i];
+    if( item==user) {
+        userFound = true;
+    }
+}
+
+// feedback users
+if(userFound == true) {
+    //check
+    console.log('Email valida, procedi con l\' accesso');
+}else {
+    console.log('Errore di autentificazione');
+}
